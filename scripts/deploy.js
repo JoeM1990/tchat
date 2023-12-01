04984678b92cb6e507da5861b0c98ce1fa4bf5e7
Joe Monkila <jnkiwa25@gmail.com>
2023-12-01 14:58:58 +0100

    create script

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+
+async function main() {
+
+  const Contract = await hre.ethers.getContractFactory("Tchat");
+  const tchat = await Contract.deploy();
+
+
+  await tchat.deployed();
+  console.log("Tchat deployed to:", tchat.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  }); 
